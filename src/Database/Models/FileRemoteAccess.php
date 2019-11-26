e9604feb238348e719e181639ed4ed2816b40775
Georgi Hristov <ghristov@gmx.de>
2019-11-26 19:01:24 +0100

    [UPDATE] improve method flexibility

--- src/Database/Models/FileRemoteAccess.php
+++ src/Database/Models/FileRemoteAccess.php
@@ -16,10 +16,10 @@ class FileRemoteAccess extends Model {
     	return (bool) self::where('file_id', $fileId)->where('token', $token)->where('expires_at', '>', date('Y-m-d H:i:s'))->count();
     }
     
-    public static function grant($fileId, $expires = self::DEFAULT_PERIOD)
+    public static function grant($file, $expires = self::DEFAULT_PERIOD)
     {
 		return self::create([
-				'file_id' => $fileId,
+				'file_id' => is_numeric($file)? $file: $file->id,
 				'token' => md5(uniqid(rand(), true)),
 				'created_by' => Auth::id()?: 0,
 				'expires_at' => date('Y-m-d H:i:s', strtotime($expires)),
